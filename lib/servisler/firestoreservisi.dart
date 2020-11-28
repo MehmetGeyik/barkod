1f8c4fe27348cc2311d9c0bad10eee24d322d662
Mehmet Geyik <mehmetgeyik1903@gmail.com>
2020-11-28 15:53:52 +0300

    firebase update.

---
+++ lib/servisler/firestoreservisi.dart
@@ -0,0 +1,26 @@
+import 'package:barkod/modeller/kullanici.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireStoreServisi {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final DateTime zaman = DateTime.now(); 
+
+  Future<void> kullaniciOlustur({id, email, kullaniciAdi, fotoUrl=""}) async {
+    await _firestore.collection("kullanicilar").doc(id).set({
+      "kullaniciAdi": kullaniciAdi,
+      "email": email,
+      "fotoUrl": fotoUrl,
+      "hakkinda": "",
+      "olusturulmaZamani": zaman
+    });
+  }
+
+  Future<Kullanici> kullaniciGetir(id) async {
+    DocumentSnapshot doc = await _firestore.collection("kullanicilar").doc(id).get();
+    if(doc.exists){
+      Kullanici kullanici = Kullanici.dokumandanUret(doc);
+      return kullanici;
+    }
+    return null;
+  }
+}
